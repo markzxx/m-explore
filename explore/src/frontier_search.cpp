6dfe2e344cbd28375944521be0b834f212a62f70
Boris Chen <boris.chen.czy@gmail.com>
2019-06-12 05:12:27 +0800

    new bug

--- explore/src/frontier_search.cpp
+++ explore/src/frontier_search.cpp
@@ -25,7 +25,7 @@ FrontierSearch::FrontierSearch(costmap_2d::Costmap2D* costmap,
 }
 
 void FrontierSearch::reset(){
-  bfs.clear();
+  bfs = queue<unsigned int>();
   visited_flag.clear();
   frontier_flag.clear();
   frontiers.clear();
