6dedd6dd83b821df87bcb7155cc6fb16c3ef217e
MARKZXX\Administrator <mark.zxx@qq.com>
2019-06-11 18:35:57 +0800

    1

--- explore/src/explore.cpp
+++ explore/src/explore.cpp
@@ -232,7 +232,7 @@ void Explore::makePlan()
 
   // send goal to move_base if we have something new to pursue
   move_base_msgs::MoveBaseGoal goal;
-  goal.target_pose.pose.position = target_position;
+  goal.target_pose.pose.position = geometry_msgs::Point(0, 0);
   goal.target_pose.pose.orientation.w = 1.;
   goal.target_pose.header.frame_id = costmap_client_.getGlobalFrameID();
   goal.target_pose.header.stamp = ros::Time::now();
